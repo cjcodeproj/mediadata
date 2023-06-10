4cf3a4411bbb87f43320662c413eebdf53aa8311
Chris J <86578804+cjcodeproj@users.noreply.github.com>
2023-06-10 06:22:39 -0500

    Updates to testing code.

--- bin/test_include.sh
+++ bin/test_include.sh
@@ -1,34 +1,63 @@
 #!/bin/sh
 
+
+#
+# Test XML for validity
 testxml() 
 {
-        xmllint --xinclude --path $XPATH --noout --schema media-schema.xsd $1 --noout >/dev/null 2>&1
+        xmllint --xinclude --path $XSDPATH --noout --schema media-schema.xsd $1 --noout >/dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		echo " ERROR -- Validation error found in $1"
 		((ERRORS = $ERRORS +1))
+		xmllint --xinclude --path $XPATH --noout --schema media-schema.xsd $1
 	fi
 
 }
 
+
+#
+# Tests for presence of double quotes vs. single quotes
+# (They're not bad, it's more of just a style issue)
 testquotes()
 {
 	grep -q '"' $1
 	if [ $? -eq 0 ]; then
-		echo "   ERROR --  Double quote found in $1"
-		((ERRORS = $ERRORS +1))
+		echo "   WARNING --  Double quote found in $1"
+		((WARNINGS = $WARNINGS +1))
 		((IC = $IC + 1))
 	fi
 }
 
+
+#
+# Test for trailing whitespace
+# (not bad, but technically increases the size of the file needlessly)
+testtrailingws()
+{
+	egrep -q " +$" $1
+	if [ $? -eq 0 ]; then
+		echo "  WARNING -- Trailing whitespace found in $1"
+		((WARNINGS = $WARNINGS +1))
+	fi
+}
+
+
+#
+# Test for instance element in the XML
+# (prevents user from accidentally saving possible personal data)
 testinstances()
 {
 	xmllint --xpath "//*[local-name()='instances']" --noout $1 >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo "   ERROR -- Instances element found in $1"
-		((ERRORS = $ERRORS +1))
+		((WARNINGS = $WARNINGS +1))
 	fi
 }
 
+
+#
+# Test for comments
+# (Comments aren't bad, but you may not want to commit them)
 testcomments()
 {
 	grep -q '<!--' $1
@@ -38,6 +67,10 @@ testcomments()
 	fi
 }
 
+
+#
+# Test for keywords (experimental)
+# See how many keywords the movie had
 testkeywords()
 {
 	xmllint --xpath $XPATH --noout $1 >/dev/null 2>&1
